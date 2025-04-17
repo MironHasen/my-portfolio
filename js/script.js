f051cbb6f7208da1d7b89264dde342a884229a26
MironHasen <mir0n.hasen@gmail.com>
2025-04-17 16:28:07 +0600

    added many things

--- js/script.js
+++ js/script.js
@@ -182,3 +182,47 @@ sr.reveal('.contacts-container',{delay: 200});
 // 
 // scrool footer
 sr.reveal('.contacts-container',{delay: 200});
+
+// web3form form scripts
+const form = document.getElementById('form');
+const result = document.getElementById('result'); 
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    result.innerHTML = "Please wait..."
+    result.className = "wait";
+
+    fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+            'Content-Type': "application/json",
+            'Accept': "application/json"
+        },
+        body: json
+    })
+    .then(async (response) => {
+        let json =  await response.json();
+        if (response.status === 200) {
+            result.innerHTML = "Thanks for your submission";
+            result.className ="success";
+            form.reset();
+        } else {
+            console.log(response);
+            result.innerHTML = json.message;
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        result.innerHTML = "Oops! There was a problem submitting your form.";
+        result.error = "error";
+    })
+    .then(function() {
+        form.reset();
+        setTimeout(() => {
+            result.style.display = "none";
+        }, 3000);
+    });
+})
